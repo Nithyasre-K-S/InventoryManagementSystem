ea0aa11cd595c31aaf685684a8944564f0b5a57c
HemaR-03 <hema.r@capgemini.com>
2026-04-01 19:40:33 +0530

    Delete IMS/backend/IMS backend directory

--- IMS/backend/IMS backend/InventoryManagementSystem/project/src/main/java/com/project/inventory/exceptionhandling/InventoryException.java
+++
@@ -1,7 +0,0 @@
-package com.project.inventory.exceptionhandling;
-
-public class InventoryException extends RuntimeException {
-	public InventoryException( String message) {
-		super(message);
-	}
-}
